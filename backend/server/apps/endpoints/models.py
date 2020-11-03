def4474a778cbeee44ba9aac4b398d2bf677b4ba
Helio Quaresma Jr <juniorhelio004@gmail.com>
2020-11-03 14:54:27 -0300

    endpoints model

--- backend/server/apps/endpoints/models.py
+++ backend/server/apps/endpoints/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 
-class Endpoints(models.Model):
+class Endpoint(models.Model):
 
     name = models.CharField(max_length=128)
     owner = models.CharField(max_length=128)
     created_at = models.DateTimeField(auto_now_add=True, blank=True)
 
 
-class MLALgorithm(models.Model):
+class MLAlgorithm(models.Model):
 
     name = models.CharField(max_length=128)
     description = models.CharField(max_length=1000)
@@ -15,16 +15,16 @@ class MLALgorithm(models.Model):
     version = models.CharField(max_length=128)
     owner = models.CharField(max_length=128)
     created_at = models.DateTimeField(auto_now_add=True, blank=True)
-    parent_endpoint = models.ForeignKey(Endpoints, on_delete=models.CASCADE)
+    parent_endpoint = models.ForeignKey(Endpoint, on_delete=models.CASCADE)
 
 
-class MLALgorithmStatus(models.Model):
+class MLAlgorithmStatus(models.Model):
 
     status = models.CharField(max_length=128)
     active = models.BooleanField()
     created_by = models.CharField(max_length=128)
     created_at = models.DateTimeField(auto_now_add=True, blank=True)
-    parent_mlalgorithm = models.ForeignKey(MLALgorithm, on_delete=models.CASCADE, related_name= "status")
+    parent_mlalgorithm = models.ForeignKey(MLAlgorithm, on_delete=models.CASCADE, related_name= "status")
 
 
 class MLRequest(models.Model):
@@ -34,4 +34,4 @@ class MLRequest(models.Model):
     response = models.CharField(max_length=10000)
     feedback = models.CharField(max_length=10000)
     created_at = models.DateTimeField(auto_now_add=True, blank=True)
-    parent_mlalgorithm = models.ForeignKey(MLALgorithm, on_delete=models.CASCADE)
+    parent_mlalgorithm = models.ForeignKey(MLAlgorithm, on_delete=models.CASCADE)
